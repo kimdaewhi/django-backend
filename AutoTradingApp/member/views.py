049bf63de2ec7f914c84ce4f05c75c0cb5716a32
김대휘 <kimdaewhi@naver.com>
2024-07-18 22:17:31 +0900

    App key, secret 추가

--- AutoTradingApp/member/views.py
+++ AutoTradingApp/member/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def login(request):
+    # APP KEY : PS2ZuCSGIUOU4R0M3UYVxaWsDMSYYecvAtYV
+    # APP SECRET : DZxy0nVMEmkDEaEg4bVqmpjA4z+eWQ6kZ/z4hs68UGKgSP/GRIQ9xPqW01hQba15Jx7L73snAAdfJ+iiyypXuRDgrppTgWWtVg84BGzxHQFf60E3YxMyX1GTizCzUV4Zsns40rUwaZYVHYOpXuwcWVyL9sEEazNY+caPNc4iE17KfwEtGM4=
     if request.method == 'POST':
         key = request.POST.get('app_key', '')  # txt_token 필드에서 입력된 값을 가져옵니다.
         secret = request.POST.get('app_secret', '')
