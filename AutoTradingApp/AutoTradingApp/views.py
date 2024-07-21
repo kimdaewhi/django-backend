a9c14a94b463cc4df1f5b50ad7052c218237c804
김대휘 <kimdaewhi@naver.com>
2024-07-21 15:38:07 +0900

    Member Model 적용

--- AutoTradingApp/AutoTradingApp/views.py
+++ AutoTradingApp/AutoTradingApp/views.py
@@ -10,7 +10,8 @@ from django.utils import timezone
 def login(request):
     if request.method == 'POST':
         # 1. Key, Secret 코드를 기반으로 요청 url 생성
-        key = request.POST.get('app_key', '')  # txt_token 필드에서 입력된 값을 가져옵니다.
+        id = request.POST.get('regID', '')
+        key = request.POST.get('app_key', '')
         secret = request.POST.get('app_secret', '')
         
         api_url = 'https://openapivts.koreainvestment.com:29443/oauth2/tokenP'
@@ -31,21 +32,24 @@ def login(request):
                 'api_response': api_response,
             }
             
-            # 1. Member Model 객체 생성
+            # 2. Member Model 객체 생성
             access_token = api_response.get('access_token')
             expired_date = api_response.get('access_token_token_expired')
             token_type = api_response.get('token_type')
             expires_in = api_response.get('expires_in')
             
             member = Member(
-                loginId = key,
+                loginId = id,
                 access_token = access_token,
                 expired_date = expired_date,
                 access_token_type = token_type,
                 expires_in = expires_in
             )
             
-            print(member)
+            # 3. Member 객체를 저장하지 않고 바로 사용(임시)
+            context = {
+                'member': member
+            }
             
             return render(request, 'AutoTradingApp/main.html', context)
         else:
